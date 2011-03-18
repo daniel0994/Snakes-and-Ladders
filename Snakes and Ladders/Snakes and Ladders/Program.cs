674874845dd58184f91a19969a1f52fbb0066938
ursa-arcadius <sourceforge@grosinger.net>
2011-03-17 22:06:43 -0700

    Generated new Solution and project

---
+++ Snakes and Ladders/Snakes and Ladders/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Snakes_and_Ladders
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
